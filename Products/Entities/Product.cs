f7d0e949587cb778d6105a7a947eb127e4d04ec6
Mikko Vornanen <mikko.vornanen@mvcvidera.com>
2025-01-25 10:26:36 +0200

    Added Product entity

---
+++ Products/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Products.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
